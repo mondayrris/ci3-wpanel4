9f9c7cbcf2e106ac23672e29cd50c4aeb8fc83ee
Mondayrris <mondayrris@gmail.com>
2022-02-10 16:33:03 +0800

    migrate sqlite to mysql

--- public/config/database.php
+++ public/config/database.php
@@ -72,10 +72,54 @@
 $active_group = ENVIRONMENT;
 $query_builder = TRUE;
 
+$db['development'] = array(
+    'dsn' => '',
+    'hostname' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'database' => 'ci3-wpanel4',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
+
+$db['production'] = array(
+    'dsn' => '',
+    'hostname' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'database' => 'ci3-wpanel4',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
+
 /**
  * Setup to development environment.
  */
-$db['development'] = array(
+$db['development_sqlite3'] = array(
     'dsn' => '',
     'hostname' => '',
     'username' => '',
@@ -100,7 +144,7 @@ $db['development'] = array(
 /**
  * Setup to production environment.
  */
-$db['production'] = array(
+$db['production_sqlite3'] = array(
     'dsn' => '',
     'hostname' => '',
     'username' => '',
